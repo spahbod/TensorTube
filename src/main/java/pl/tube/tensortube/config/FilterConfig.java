5e59d654e9b9dbd7a5f35c8e3c369ce9697cdfbc
spahbod <spahbod@hotmail.com>
2020-07-19 20:23:29 +0200

    Porządki importów.

--- src/main/java/pl/tube/tensortube/config/FilterConfig.java
+++ src/main/java/pl/tube/tensortube/config/FilterConfig.java
@@ -1,12 +1,9 @@
 package pl.tube.tensortube.config;
 
-import org.primefaces.webapp.filter.FileUploadFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.tube.tensortube.filter.LoginFilter;
-
-import javax.servlet.Filter;
 
 @Configuration
 public class FilterConfig {
--- src/main/java/pl/tube/tensortube/filter/LoginFilter.java
+++ src/main/java/pl/tube/tensortube/filter/LoginFilter.java
@@ -2,7 +2,6 @@ package pl.tube.tensortube.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import pl.tube.tensortube.controller.UserManager;
 
 import javax.servlet.*;
